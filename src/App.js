6492da71fc15972ef75824f5467fbec243fc7a7a
Ashborn <80640045+Burned-to@users.noreply.github.com>
2022-09-17 20:46:22 +0430

    initialising project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+import Header from './components/Header';
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackList feedback={feedback} />
+      </div>
+    </>
   );
 }
 
